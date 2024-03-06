cc3531f3b85622ec7bde1c33c45d21e065c3f6e7
Richard Rosario <suparico91@gmail.com>
2024-03-06 07:58:26 +0000

    updating tests to dynamically test for both an empty string and the name string. Hello.go currently fails test will fix soon as i have ran out of time for today

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Rich")
+  t.Run("saying hello to people", func(t *testing.T){
+  got := Hello("Rich")
 	want := "Hello, Rich"
+  assertCorrectMessage(t,got,want)
+})
+t.Run("empty string defaults to world", func(t *testing.T){
+  got := Hello("")
+  want := "Hello, world"
+  assertCorrectMessage(t,got,want)
+})
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+}
+
+func assertCorrectMessage(t testing.TB, got, want string){
+  t.Helper()
+  if got != want{
+    t.Errorf("got %q want %q", got, want)
+  }
 }
