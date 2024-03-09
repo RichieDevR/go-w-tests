ccabb6658da7be1da1dca9edbb50fc9be6f182c5
richiedevr <richie.rosario@protonmail.com>
2024-03-08 19:56:14 -0500

    updated hello.go to pass tests

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,31 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	
-	t.Run("saying hello to people", func(t *testing.T){
+	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Rich")
-	
+
 		want := "Hello, Rich"
-  	
-		assertCorrectMessage(t,got,want
-})
-	t.Run("empty string defaults to world", func(t *testing.T){
-		
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to world", func(t *testing.T) {
 		got := Hello("")
-		
-		want := "Hello, world"
-		
-		assertCorrectMessage(t,got,want)
+
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
 	})
 }
 
-func assertCorrectMessage(t testing.TB, got, want string){
-	
+func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
-	
-	if got != want{
-		
+
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
-	
 	}
 }
