b0b2a735c8e4d7cb98df3ef516282d0295faaabc
richiedevr <richie.rosario@protonmail.com>
2024-03-20 02:46:37 -0400

    updated tests to test for array slices and refactored to passed tests

--- arrays-and-slices/arraysum_test.go
+++ arrays-and-slices/arraysum_test.go
@@ -3,11 +3,38 @@ package main
 import "testing"
 
 func TestSumArray(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := SumArray(numbers)
-	want := 15
+	t.Run("collection of exactly 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d given the array [numbers]int %v ", got, want, numbers)
-	}
+		var sum int
+
+		for _, num := range numbers {
+			sum += num
+		}
+
+		got := SumArray(numbers)
+
+		want := sum
+
+		if got != want {
+			t.Errorf("got %d want %d given the array [numbers]int %v ", got, want, numbers)
+		}
+	})
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		var sum int
+
+		for i := 0; i < len(numbers); i++ {
+			sum += numbers[i]
+		}
+
+		got := SumArray(numbers)
+
+		want := sum
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
 }
