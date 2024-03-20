34728bef0f3e48815bb2cc92c59cf0254c1d6deb
richiedevr <richie.rosario@protonmail.com>
2024-03-20 02:57:45 -0400

    removed redundant test and refactored to passed tests

--- arrays-and-slices/arraysum_test.go
+++ arrays-and-slices/arraysum_test.go
@@ -3,38 +3,18 @@ package main
 import "testing"
 
 func TestSumArray(t *testing.T) {
-	t.Run("collection of exactly 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
+	var sum int
 
-		var sum int
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[i]
+	}
 
-		for _, num := range numbers {
-			sum += num
-		}
+	got := SumArray(numbers)
 
-		got := SumArray(numbers)
+	want := sum
 
-		want := sum
-
-		if got != want {
-			t.Errorf("got %d want %d given the array [numbers]int %v ", got, want, numbers)
-		}
-	})
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		var sum int
-
-		for i := 0; i < len(numbers); i++ {
-			sum += numbers[i]
-		}
-
-		got := SumArray(numbers)
-
-		want := sum
-
-		if got != want {
-			t.Errorf("got %d want %d given %v", got, want, numbers)
-		}
-	})
+	if got != want {
+		t.Errorf("got %d want %d given %v", got, want, numbers)
+	}
 }
