da38ea7275db3153cfaea83632449908760a4b55
richiedevr <richie.rosario@protonmail.com>
2024-03-20 01:46:36 -0400

    changed param name

--- arrays-and-slices/arraysum.go
+++ arrays-and-slices/arraysum.go
@@ -1,8 +1,8 @@
 package main
 
-func SumArray(nums [5]int) int {
+func SumArray(array [5]int) int {
 	sum := 0
-	for _, num := range nums {
+	for _, num := range array {
 		sum += num
 	}
 	return sum
